94dcd4e202e99ce03586fa71599e530f2d2549fc
LandyTan <547682533@qq.com>
2021-06-24 14:16:53 +0800

    新增ExceptionNotify

---
+++ TxLibrary/通知机制/ExceptionNotify/ExceptionNotifyBase.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * File:        ExceptionNotifyBase.cs
+ * Author:      Landy
+ * Date:        2021年6月24日
+ * Description：异常通知机制
+ * History
+ * <Date>           <Author>    <Description>
+ * 2021年6月24日    Landy        创建
+ */
+using System;
+
+namespace TxLibrary.Notification
+{
+    /// <summary>
+    /// 异常通知基类
+    /// </summary>
+    public abstract class ExceptionNotifyBase : IExceptionNotify
+    {
+        /// <summary>
+        /// 触发异常通知事件
+        /// </summary>
+        public event TriggerExceptionHandle TriggerExceptionEvent;
+
+        /// <summary>
+        /// 存储异常实例对象
+        /// </summary>
+        private Exception _exception;
+
+        /// <summary>
+        /// 获取异常
+        /// </summary>
+        /// <returns></returns>
+        public Exception GetException()
+        {
+            return _exception;
+        }
+
+        /// <summary>
+        /// 清除异常
+        /// </summary>
+        public void ClearException()
+        {
+            _exception = null;
+        }
+
+        /// <summary>
+        /// 检查是否存在异常
+        /// </summary>
+        /// <returns></returns>
+        public bool IsException()
+        {
+            return !(_exception is null);
+        }
+
+        /// <summary>
+        /// 触发异常
+        /// </summary>
+        /// <param name="e"></param>
+        protected void TriggerException(Exception e)
+        {
+            _exception = e;
+            TriggerExceptionEvent?.Invoke(this, _exception);
+        }
+    }
+}
+
---
+++ TxLibrary/通知机制/ExceptionNotify/IExceptionNotify.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TxLibrary.Notification
+{
+    /// <summary>
+    /// 异常触发事件
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="exception"></param>
+    public delegate void TriggerExceptionHandle(object sender, Exception exception);
+
+    /// <summary>
+    /// 异常通知接口
+    /// </summary>
+    public interface IExceptionNotify
+    {
+        /// <summary>
+        /// 发生异常时触发此事件
+        /// </summary>
+        event TriggerExceptionHandle TriggerExceptionEvent;
+
+        /// <summary>
+        /// 获取异常信息
+        /// </summary>
+        /// <returns></returns>
+        Exception GetException();
+
+        /// <summary>
+        /// 清除异常
+        /// </summary>
+        void ClearException();
+
+        /// <summary>
+        /// 判断是否存在异常
+        /// </summary>
+        /// <returns></returns>
+        bool IsException();
+    }
+
+}
